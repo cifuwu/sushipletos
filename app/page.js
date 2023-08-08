6a4f13c682b8d53ec51871a93fffadd3da9b4131
cifuwu <sebastian.cifuentesm@sansano.usm.cl>
2023-08-08 18:46:28 -0400

    readme update

--- app/page.js
+++ app/page.js
@@ -2,9 +2,17 @@ import Tabla from "@/components/Tabla/Tabla";
 
 export default function Home() {
   return (
-    <main style={{marginTop: 25}}>
+    <main style={{marginTop: 45}}>
       <div className="container">
         <div className="row">
+          <div className="col">
+            <h3>
+              Registro de préstamos 
+            </h3>
+          </div>
+        </div>
+
+        <div className="row" style={{marginTop: 20}}>
           <div className="col">
             <div className="card">
               <div className="card-body">
